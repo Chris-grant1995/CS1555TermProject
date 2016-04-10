9e56166d4cec97c529105d3bc1026d885cebca6a
Chris Grant <cgrant1995@me.com>
2016-04-10 11:45:12 -0400

    Finished Sample Data

--- src/CreateSampleUsers.java
+++ src/CreateSampleUsers.java
@@ -27,7 +27,7 @@ public class CreateSampleUsers {
         dobC.add(Calendar.DATE, 1);
         System.out.println(login.format(dobC.getTime()));
         for(int i =0; i<100; i++ ){
-            String statement = "INSERT INTO Users VALUES(" + i + ',';
+            String statement = "INSERT INTO Users VALUES(UsersSEQ.nextval,";
             String fname = fnames[rand.nextInt(fnames.length)];
             String lname = lnames[rand.nextInt(lnames.length)];
             String name = fname + " " + lname;
@@ -46,8 +46,8 @@ public class CreateSampleUsers {
         }
         for(int i =0; i<200; i++){
             String statement = "INSERT INTO Friendships VALUES(";
-            String f1 = rand.nextInt(names.size()) + "";
-            String f2 = rand.nextInt(names.size()) + "";
+            String f1 = (rand.nextInt(names.size()) + 1) + "";
+            String f2 = (rand.nextInt(names.size()) + 1) + "";
             String friend = f1  + ", " + f2;
             while (friends.contains(friend) || f1.equals(f2)){
                  f1 = rand.nextInt(names.size()) + "";
@@ -57,9 +57,50 @@ public class CreateSampleUsers {
             friends.add(friend);
             friend = f2  + ", " + f1;
             friends.add(friend);
-            Boolean confirmed = rand.nextBoolean();
+            int confirmed = rand.nextInt(2);
             statement+= " " + friend + ", " + confirmed + " );";
             System.out.println(statement);
+        }
+        String[] groups = {"CS", "COE", "EE", "NeuroSci", "LibArts", "Com", "Nursing", "Rehab", "Mech E", "Premed"};
+        for(int i =0; i<groups.length; i++){
+            String statement = "INSERT INTO Groups VALUES(GroupSEQ.nextval," + "\'" + groups[i] + "\', \'";
+            int maxMems = rand.nextInt(100);
+            String desc = groups[i] + " Group";
+            statement+= desc + "\', ";
+            statement+= maxMems + ");";
+            System.out.println(statement);
+            ArrayList<Integer> members = new ArrayList<>();
+            int totalmems = rand.nextInt(maxMems);
+            //System.out.println(totalmems);
+            for(int x =0; x<totalmems; x++){
+                int num = i+1;
+                String memtable = "INSERT INTO Membership VALUES(" + num +",";
+                int user = rand.nextInt(100) + 1;
+                while(members.contains(user)){
+                    user = rand.nextInt(100) + 1;
+                }
+                members.add(user);
+                memtable += user + ");";
+                System.out.println(memtable);
+            }
+
+        }
+        for(int i =0; i<300; i++){
+            String statement = "INSERT INTO Messages VALUES(MsgSEQ.nextval,";
+            int recID = rand.nextInt(100) + 1;
+            int senID = rand.nextInt(100) + 1;
+            while(recID == senID){
+                recID = rand.nextInt(100) + 1;
+                senID = rand.nextInt(100) + 1;
+            }
+            statement+= recID + ", "+ senID + ", \'";
+            String sub = "Test Sub " + i;
+            statement+= sub + "\', \'";
+            String text = "Test text " + i;
+            statement+= text + "\', TIMESTAMP \'";
+            statement+=login.format(loginC.getTime()).toString() + "\');";
+            System.out.println(statement);
+
         }
     }
 }
